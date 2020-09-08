6bb695a232fc743586e222005cace0bfcb9e71b1
vadna034 <vadna034@umn.edu>
2020-09-07 21:32:56 -0500

    Made the login page function from fetch

--- src/login.js
+++ src/login.js
@@ -2,4 +2,35 @@ $(document).ready(() => {
   document.getElementById("registerButton").addEventListener("click", () => {
     window.location.href = "/register";
   });
+
+  document.getElementById("loginForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+    fetch("/login.html", {
+      method: "POST",
+      redirect: "follow",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        inputEmail: $("#inputEmail").val(),
+        inputPassword: $("#inputPassword").val(),
+      }),
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.redirected) {
+          window.location.href = "/dashboard/home";
+        } else if (response.status == 200) {
+          document.getElementById("successDiv").innerHTML =
+            "<div class='alert alert-warning text-center'>User not found. Please check that your email and password have been entered correctly</div>";
+        } else if (response.status == 500) {
+          document.getElementById("successDiv").innerHTML =
+            "<div class='alert alert-danger text-center'>Server Error: Please try again later</div>";
+        }
+      })
+      .catch((err) => {
+        document.getElementById("successDiv").innerHTML =
+          "<div class='alert alert-danger text-center'>Server Error: Please try again later</div>";
+      });
+  });
 });
