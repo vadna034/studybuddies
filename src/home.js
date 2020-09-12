3f7f698def017724aa5ec99f9a8f13049e2b2795
vadna034 <vadna034@umn.edu>
2020-09-12 15:18:07 -0500

    Moved home javascript to it's own file

---
+++ src/home.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  fetch("/getMeetings", { method: "POST" })
+    .then((response) => response.text())
+    .then((response) => {
+      console.log(response);
+      var events = JSON.parse(response);
+      var calEvents = [];
+      var calendarEl = document.getElementById("calendar");
+      console.log(events);
+
+      events.forEach((event) => {
+        calEvents.push({
+          title: event.code,
+          timeZone: "local",
+          start: Date.parse(event.starttime),
+          end: Date.parse(event.endtime),
+        });
+        console.log(event.starttime);
+        console.log(event.endtime);
+      });
+
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: "timeGridWeek",
+        events: calEvents,
+      });
+
+      calendar.render();
+    })
+    .catch((err) => {
+      alert("Could not load your meetings");
+      var dash = document.getElementsByClassName("dash");
+      console.log(dash);
+    });
+});
